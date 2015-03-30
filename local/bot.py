e71bfd5c189b654a9528dc8c14383bf363b7d53d
taygetea <taygetea@gmail.com>
2015-03-30 00:44:43 +0000

    first. for real.

---
+++ local/bot.py
@@ -0,0 +1,1 @@
+/root/ircbot/bot.py
---
+++ recoveredbot.py
@@ -0,0 +1,57 @@
+from twisted.words.protocols import irc
+from twisted.internet import protocol
+import re
+
+
+class IrcBot(irc.IRCClient):
+    def _get_nickname(self):
+        return self.factory.nickname
+    nickname = property(_get_nickname)
+
+    def signedOn(self):
+        self.join(self.factory.channel)
+        print "Signed on as %s." % (self.nickname,)
+
+    def joined(self, channel):
+        print "Joined %s." % (channel,)
+
+    def privmsg(self, user, channel, msg):
+        if not user:
+            return
+        if self.username in msg:
+            msg = re.compile(self.nickname + "[:,]* ?", re.I).sub('', msg)
+            prefix = "%s: " % (user.split('!', 1)[0], )
+            self.msg(self.factory.channel, prefix + ": this is a message")
+        else:
+            prefix = ''
+
+
+class IrcBotFactory(protocol.ClientFactory):
+    protocol = IrcBot
+
+    def __init__(self, channel, nickname='LWBot'):
+        self.channel = channel
+        self.nickname = nickname
+
+    def clientConnectionLost(self, connector, reason):
+        print "Lost connection (%s), reconnecting." % (reason,)
+        connector.connect()
+
+    def clientConnectionFailed(self, connector, reason):
+        print "Could not connect: %s" % (reason,)
+
+import sys
+from twisted.internet import reactor
+
+if __name__ == "__main__":
+    chan = sys.argv[1]
+    reactor.connectTCP('irc.freenode.net', 6667, IrcBotFactory('#' + chan))
+    reactor.run()
+
+
+
+
+
+
+
+
