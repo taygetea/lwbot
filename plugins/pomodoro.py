b05da61629da4e55098c7532d7ec45f3d2d11e26
taygetea <taygetea@gmail.com>
2015-03-29 22:55:16 -0400

    Create pomodoro.py

---
+++ plugins/pomodoro.py
@@ -0,0 +1,11 @@
+from twisted.words.protocols import irc
+from twisted.internet import protocol
+from twisted.internet import reactor
+from bot import IrcBot
+
+def init():
+    return 'pomo'
+
+def pomo():
+    print "the 'pomo' function was called."  #placeholder
+
