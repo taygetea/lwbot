f8422f543bde8b886353f3ccff70311c5eec3d82
Olivia Schaefer <taygetea@gmail.com>
2015-09-21 23:51:42 -0700

    stuff

--- bot.py
+++ bot.py
@@ -1,10 +1,9 @@
 from twisted.words.protocols import irc
 from twisted.internet import protocol
 import re
-import plugins
+import plugins.pomodoro
 
 print plugins.modules
-
 class IrcBot(irc.IRCClient):
     def _get_nickname(self):
         return self.factory.nickname
@@ -16,19 +15,30 @@ class IrcBot(irc.IRCClient):
 
     def joined(self, channel):
         print "Joined %s." % (channel,)
-
+    """
+    < hugaraxia> When a ban is placed by an op, PM the op for a timeframe to remove the ban (accept "10m" or "2h" or "30d"), and then start timer, remove ban when time is up (+random interval to prevent gaming?). If op doesn't reply: Sane default. (5h?) (Perhaps announce ban time, possibly in ##meta:  fuzzy them to "short", "long", "very long" to prevent gaming.)
+    7839-07:12 < hugaraxia> When ban is placed, optionally on very long bans: Start a timer for 1hr: Message #lw-meta that ban discussion on <username> is now allowed.
+    """
     def privmsg(self, user, channel, msg):
         reload(plugins)
-        if not user:
-            return
+        print locals()
+        print irc.parseModes(msg)
         if msg.startswith(self.nickname):
             msg = re.compile(self.nickname + "[:,]* ?", re.I).sub('', msg)
             prefix = "%s: " % (user.split('!', 1)[0], )
-            evaluated = eval(msg)
-            self.msg(self.factory.channel, prefix + str(evaluated))
+            print msg
+            built = prefix + msg
+            self.msg(self.factory.channel, built)
         else:
             prefix = ''
 
+class Bantimer:
+    def __init__(self, op, user, channel):
+        self.operator = op
+        self.banned = user
+        self.channel = channel
+    def opmsg(self):
+        pass
 
 class IrcBotFactory(protocol.ClientFactory):
     protocol = IrcBot
@@ -48,6 +58,6 @@ class IrcBotFactory(protocol.ClientFactory):
 from twisted.internet import reactor
 
 if __name__ == "__main__":
-    chan = 'lw-prog'  #sys.argv[1]
-    reactor.connectTCP('irc.freenode.net', 6667, IrcBotFactory('#' + chan))
+    chan = 'bot'  #sys.argv[1]
+    reactor.connectTCP('localhost', 6667, IrcBotFactory('#' + chan))
     reactor.run()
